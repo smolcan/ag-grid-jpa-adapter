490cd43e6dc58ded8052f6c2bec02517d74f6685
smolcan <samuel.molcan01@gmail.com>
2024-12-20 14:07:41 +0100

    docs init

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 A lightweight Maven library for integrating **[AG Grid Server-Side Mode](https://ag-grid.com/angular-data-grid/server-side-model/)** with backend applications using **JPA**. This solution simplifies querying mapped entities for AG Grid and supports advanced server-side operations, including sorting, filtering, pagination, row grouping, and pivoting.
 
+<p align="center">
+   <a href="https://smolcan.github.io/ag-grid-jpa-adapter/"><strong>Explore Docs >></strong></a>
+</p>
+
 ## **⚠️ Disclaimer: Active Development**
 This project is currently in active development.
 It is not fully tested and may contain bugs or incomplete features.
--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -13,12 +13,12 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ag-grid-jpa-adapter/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'smolcan', // Usually your GitHub org/user name.
+  projectName: 'ag-grid-jpa-adapter', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
