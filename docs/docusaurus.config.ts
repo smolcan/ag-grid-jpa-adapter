c495e828e1d94e2fb57f8ec14c6b16dd5de4d33e
smolcan <samuel.molcan01@gmail.com>
2024-12-21 17:52:19 +0100

    footer

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -76,7 +76,7 @@ const config: Config = {
     },
     footer: {
       style: 'dark',
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} AG Grid JPA Adapter`,
     },
     prism: {
       theme: prismThemes.github,
