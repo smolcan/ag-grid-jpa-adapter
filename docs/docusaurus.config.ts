815df72b3a927affbc89a24e5fa0e6b4d93e84f6
smolcan <samuel.molcan01@gmail.com>
2025-04-02 16:25:50 +0200

    edit this page removed

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -40,8 +40,8 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/smolcan/ag-grid-jpa-adapter/tree/main/docs',
+          // editUrl:
+          //   'https://github.com/smolcan/ag-grid-jpa-adapter/tree/main/docs',
         },
         blog: false,
         theme: {
