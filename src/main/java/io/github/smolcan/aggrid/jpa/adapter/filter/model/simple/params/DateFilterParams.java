e5b0942256400f6ee59e2981bee3c796ab3212c8
smolcan <samuel.molcan01@gmail.com>
2025-05-29 13:47:37 +0200

    scalar filter params builder inheritence problem fix

--- src/main/java/io/github/smolcan/aggrid/jpa/adapter/filter/model/simple/params/DateFilterParams.java
+++ src/main/java/io/github/smolcan/aggrid/jpa/adapter/filter/model/simple/params/DateFilterParams.java
@@ -110,5 +110,41 @@ public class DateFilterParams extends ScalarFilterParams {
             this.minValidYear = minValidYear;
             return this;
         }
+
+        @Override
+        public Builder inRangeInclusive(boolean inRangeInclusive) {
+            super.inRangeInclusive(inRangeInclusive);
+            return this;
+        }
+
+        @Override
+        public Builder includeBlanksInEquals(boolean includeBlanksInEquals) {
+            super.includeBlanksInEquals(includeBlanksInEquals);
+            return this;
+        }
+
+        @Override
+        public Builder includeBlanksInNotEqual(boolean includeBlanksInNotEqual) {
+            super.includeBlanksInNotEqual(includeBlanksInNotEqual);
+            return this;
+        }
+
+        @Override
+        public Builder includeBlanksInLessThan(boolean includeBlanksInLessThan) {
+            super.includeBlanksInLessThan(includeBlanksInLessThan);
+            return this;
+        }
+
+        @Override
+        public Builder includeBlanksInGreaterThan(boolean includeBlanksInGreaterThan) {
+            super.includeBlanksInGreaterThan(includeBlanksInGreaterThan);
+            return this;
+        }
+
+        @Override
+        public Builder includeBlanksInRange(boolean includeBlanksInRange) {
+            super.includeBlanksInRange(includeBlanksInRange);
+            return this;
+        }
     }
 }
--- src/main/java/io/github/smolcan/aggrid/jpa/adapter/filter/model/simple/params/NumberFilterParams.java
+++ src/main/java/io/github/smolcan/aggrid/jpa/adapter/filter/model/simple/params/NumberFilterParams.java
@@ -11,10 +11,46 @@ public class NumberFilterParams extends ScalarFilterParams {
     }
     
     public static class Builder extends ScalarFilterParams.Builder {
-        
+
+        @Override
+        public Builder inRangeInclusive(boolean inRangeInclusive) {
+            super.inRangeInclusive(inRangeInclusive);
+            return this;
+        }
+
+        @Override
+        public Builder includeBlanksInEquals(boolean includeBlanksInEquals) {
+            super.includeBlanksInEquals(includeBlanksInEquals);
+            return this;
+        }
+
+        @Override
+        public Builder includeBlanksInNotEqual(boolean includeBlanksInNotEqual) {
+            super.includeBlanksInNotEqual(includeBlanksInNotEqual);
+            return this;
+        }
+
+        @Override
+        public Builder includeBlanksInLessThan(boolean includeBlanksInLessThan) {
+            super.includeBlanksInLessThan(includeBlanksInLessThan);
+            return this;
+        }
+
+        @Override
+        public Builder includeBlanksInGreaterThan(boolean includeBlanksInGreaterThan) {
+            super.includeBlanksInGreaterThan(includeBlanksInGreaterThan);
+            return this;
+        }
+
+        @Override
+        public Builder includeBlanksInRange(boolean includeBlanksInRange) {
+            super.includeBlanksInRange(includeBlanksInRange);
+            return this;
+        }
+
+        @Override
         public NumberFilterParams build() {
             return new NumberFilterParams(this);
         }
-        
     }
 }
