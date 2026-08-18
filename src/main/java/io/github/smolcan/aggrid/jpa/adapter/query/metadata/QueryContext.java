e85e03856d5d954ed1c18eac410883165a4a4e12
smolcan <samuel.molcan01@gmail.com>
2026-08-18 12:19:28 +0200

    define coldef with expression instead of path

--- src/main/java/io/github/smolcan/aggrid/jpa/adapter/query/metadata/QueryContext.java
+++ src/main/java/io/github/smolcan/aggrid/jpa/adapter/query/metadata/QueryContext.java
@@ -19,8 +19,6 @@ import java.util.*;
  */
 @Getter
 @Setter
-@Builder(toBuilder = true)
-@AllArgsConstructor
 public class QueryContext<E> {
     
     @NonNull
@@ -31,24 +29,18 @@ public class QueryContext<E> {
     private final Root<E> root;
     
     @NonNull
-    @Builder.Default
     private List<SelectionMetadata> selections = new ArrayList<>();
     @NonNull
-    @Builder.Default
     private List<WherePredicateMetadata> wherePredicates = new ArrayList<>();
     @NonNull
-    @Builder.Default
     private List<GroupingMetadata> grouping = new ArrayList<>();
     @NonNull
-    @Builder.Default
     private List<HavingMetadata> having = new ArrayList<>();
     @NonNull
-    @Builder.Default
     private List<OrderMetadata> orders = new ArrayList<>();
     private int firstResult;
     private int maxResults;
     @NonNull
-    @Builder.Default
     private PivotingContext pivotingContext = new PivotingContext();
 
     public QueryContext(@NonNull CriteriaBuilder criteriaBuilder, @NonNull AbstractQuery<?> query, @NonNull Root<E> root) {
